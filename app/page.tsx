67cbe089449c77103c8c45bf991b730360d48d92
ShumailaNaz <devshum@outlook.com>
2025-05-09 02:47:50 -0700

    redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   return (
     
-    <main className="p-6 grid gap-6 grid-cols-1 md:grid-cols-2">
-    <section className="col-span-1 md:col-span-2">
-      <h1 className="text-2xl font-bold mb-4 cursor-pointer">Financial Dashboard</h1>
-    </section>
-
-   
-  </main>
+    redirect("/dashboard")
   );
 }
